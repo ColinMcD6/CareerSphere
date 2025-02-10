d643419c575b0b2a745c06ce1df3a6acd87af450
Colin McDonell <colinmmcdonell@gmail.com>
2025-02-10 10:25:20 -0600

    At minute 32 in video

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,39 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectToDatabase from "./config/db";
+import { NODE_ENV, PORT, APP_ORIGIN } from "./constants/env";
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        status: "healthy",
-    });
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(
+    cors({
+        origin: APP_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+
+app.get("/", 
+    catchErrors(async (req, res, next) => {
+        throw new Error("This is an test error");
+        res.status(200).json({
+            status: "healthy", 
+        });
+    })
+);
+
+app.use(errorHandler);
 
 app.listen(
     4004,
-    () => {
-        console.log('Server is running on port 4004 in development environment. ');
+    async () => {
+        console.log(`Server is running on port ${PORT} in ${NODE_ENV} environment.`);
+        await connectToDatabase();
     }
 );
