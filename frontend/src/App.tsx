7f8070ecba90437da0d5b81c49c426e89aa9db61
Your Name <quickttsales@gmail.com>
2025-02-15 18:54:51 -0600

    Employer Portal #83 	Starting the basic frame that will allow an employer to create new job posting.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Welcome from "./pages/Welcome"
+import CreateJobsPosting from "./pages/CreateJobPositing"
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/welcome" element={<Welcome />} />
+            <Route path="/Create-Job-Posting" element={<CreateJobsPosting />} / >
         </Routes>
     )
 }
