559a3d087464eeac60c18eb03851abb94c44a1a9
EthanLapkin <126340807+EthanLapkin@users.noreply.github.com>
2025-02-16 17:24:14 -0600

    added radios to signup

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -6,26 +6,50 @@ const SignUp = () => {
   const handleBack = async () => {
     navigate("/");
   };
+  //TODO, make the radios actually swap
   let out = (
     <>
       <form>
-        <div className="mb-3">
+        <div className="emailDiv">
           <label htmlFor="inputEmail" className="form-label">
             Email address
           </label>
           <input type="email" className="form-control" id="inputEmail" />
         </div>
-        <div className="mb-3">
+        <div className="nameDiv">
           <label htmlFor="inputName" className="form-label">
             Name
           </label>
           <input type="name" className="form-control" id="inputName" />
         </div>
-        <div className="mb-3">
+        <div className="passwordDiv">
           <label htmlFor="inputPassword" className="form-label">
             Password
           </label>
           <input type="password" className="form-control" id="inputPassword" />
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault1">
+            Candidate
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault2">
+            Employer
+          </label>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
