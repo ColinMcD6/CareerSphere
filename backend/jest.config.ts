f85bfc42334b8ce21780767f8dc6b2ad6733b07f
Colin McDonell <colinmmcdonell@gmail.com>
2025-02-22 17:59:00 -0600

    Test Jest

---
+++ backend/jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node', // Use 'jsdom' for frontend tests
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
+};
+  
