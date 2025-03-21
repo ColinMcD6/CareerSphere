f2a31f46bd8800714f8cca2d44a74de1c98806cc
EthanLapkin <126340807+EthanLapkin@users.noreply.github.com>
2025-03-20 22:30:18 -0500

    recategorized integration tests, wrote more unit tests, updated test plan

--- backend/src/tests/preferences.test.ts
+++ backend/src/tests/preferences.test.ts
@@ -149,7 +149,7 @@ describe('Test user preference updates', () => {
         expect(findUpdatedUser?.preferences[5]).toBe(10);
     });
 
-    test('Check if updateUserDetails will not update negative preferences', async () => {
+    test('Ensure updateUserDetails will not update negative preferences', async () => {
         const newuser = await UserModel.create({
             username: "colin", 
             email: "colin@gmail.com", 
@@ -192,7 +192,7 @@ describe('Test user preference updates', () => {
         expect(findUpdatedUser?.preferences[5]).toBe(0);
     });
 
-    test('Check if updateUserDetails will not update out of bounds preferences', async () => {
+    test('Ensure updateUserDetails will not update out of bounds preferences', async () => {
         const newuser = await UserModel.create({
             username: "colin", 
             email: "colin@gmail.com", 
@@ -235,7 +235,7 @@ describe('Test user preference updates', () => {
         expect(findUpdatedUser?.preferences[5]).toBe(0);
     });
 
-    test('Check if updateUserDetails will not update on char input', async () => {
+    test('Ensure updateUserDetails will not update on char input', async () => {
         const newuser = await UserModel.create({
             username: "colin", 
             email: "colin@gmail.com", 
@@ -278,7 +278,7 @@ describe('Test user preference updates', () => {
         expect(findUpdatedUser?.preferences[5]).toBe(0);
     });
 
-    test('Check if updateUserDetails will not update on String input', async () => {
+    test('Ensure updateUserDetails will not update on String input', async () => {
         const newuser = await UserModel.create({
             username: "colin", 
             email: "colin@gmail.com", 
@@ -321,7 +321,176 @@ describe('Test user preference updates', () => {
         expect(findUpdatedUser?.preferences[5]).toBe(0);
     });
 
-    test('Check if sort behaves ok on single job', async () => {
+    test('Ensure updateUserDetails will not update on null input', async () => {
+        const newuser = await UserModel.create({
+            username: "colin", 
+            email: "colin@gmail.com", 
+            password: "12345678", 
+            userRole: "Candidate",
+            education: ["University of Manitoba"],
+            skills: ["Kendama"],
+            experience: ["Software Engineering 2"],
+            companyDetails: undefined,
+            hiringDetails: undefined,
+        });
+        let user = await UserModel.findById(newuser._id);
+        // Check record is in database
+        expect(user).not.toBeNull();
+        const preference1 = null;
+        const preference2 = null;
+        const mReq1: Partial<Request> = {
+            body: {preference: preference1},
+            userId: newuser._id,
+        };
+        const mReq2: Partial<Request> = {
+            body: {preference: preference2},
+            userId: newuser._id,
+        };
+        const mJson = jest.fn().mockImplementation(() => null)
+        const mStatus = jest.fn().mockImplementation(() => ({ json: mJson }))
+        const mRes: Partial<Response> = {
+            status: mStatus
+        };
+        const mNext = jest.fn();
+        await updateUserDetails(mReq1 as Request, mRes as Response, mNext);
+        await updateUserDetails(mReq2 as Request, mRes as Response, mNext);
+        const findUpdatedUser = await UserModel.findById(newuser._id);
+        expect(findUpdatedUser?.preferences[0]).toBe(0);
+        expect(findUpdatedUser?.preferences[1]).toBe(0);
+        expect(findUpdatedUser?.preferences[2]).toBe(0);
+        expect(findUpdatedUser?.preferences[3]).toBe(0);
+        expect(findUpdatedUser?.preferences[4]).toBe(0);
+        expect(findUpdatedUser?.preferences[5]).toBe(0);
+    });
+
+    test('Make sure user preferences are individual', async () => {
+        const newuser = await UserModel.create({
+            username: "colin", 
+            email: "colin@gmail.com", 
+            password: "12345678", 
+            userRole: "Candidate",
+            education: ["University of Manitoba"],
+            skills: ["Kendama"],
+            experience: ["Software Engineering 2"],
+            companyDetails: undefined,
+            hiringDetails: undefined,
+        });
+        const newuser2 = await UserModel.create({
+            username: "colin", 
+            email: "colin2@gmail.com", 
+            password: "12345678", 
+            userRole: "Candidate",
+            education: ["University of Manitoba"],
+            skills: ["Kendama"],
+            experience: ["Software Engineering 2"],
+            companyDetails: undefined,
+            hiringDetails: undefined,
+        });
+        let user = await UserModel.findById(newuser._id);
+        let user2 = await UserModel.findById(newuser2._id);
+        // Check record is in database
+        expect(user).not.toBeNull();
+        expect(user2).not.toBeNull();
+        const preference1 = 2;
+        const preference2 = 4;
+        const mReq1: Partial<Request> = {
+            body: {preference: preference1},
+            userId: newuser._id,
+        };
+        const mReq2: Partial<Request> = {
+            body: {preference: preference2},
+            userId: newuser._id,
+        };
+        const mJson = jest.fn().mockImplementation(() => null)
+        const mStatus = jest.fn().mockImplementation(() => ({ json: mJson }))
+        const mRes: Partial<Response> = {
+            status: mStatus
+        };
+        const mNext = jest.fn();
+        await updateUserDetails(mReq1 as Request, mRes as Response, mNext);
+        await updateUserDetails(mReq2 as Request, mRes as Response, mNext);
+        const findUpdatedUser = await UserModel.findById(newuser._id);
+        const findUpdatedUser2 = await UserModel.findById(newuser2._id);
+        expect(findUpdatedUser?.preferences[0]).toBe(0);
+        expect(findUpdatedUser?.preferences[1]).toBe(0);
+        expect(findUpdatedUser?.preferences[2]).toBe(1);
+        expect(findUpdatedUser?.preferences[3]).toBe(0);
+        expect(findUpdatedUser?.preferences[4]).toBe(1);
+        expect(findUpdatedUser?.preferences[5]).toBe(0);
+        expect(findUpdatedUser2?.preferences[0]).toBe(0);
+        expect(findUpdatedUser2?.preferences[1]).toBe(0);
+        expect(findUpdatedUser2?.preferences[2]).toBe(0);
+        expect(findUpdatedUser2?.preferences[3]).toBe(0);
+        expect(findUpdatedUser2?.preferences[4]).toBe(0);
+        expect(findUpdatedUser2?.preferences[5]).toBe(0);
+    });
+
+    test('See if multiple accounts can be updated in concert', async () => {
+        const newuser = await UserModel.create({
+            username: "colin", 
+            email: "colin@gmail.com", 
+            password: "12345678", 
+            userRole: "Candidate",
+            education: ["University of Manitoba"],
+            skills: ["Kendama"],
+            experience: ["Software Engineering 2"],
+            companyDetails: undefined,
+            hiringDetails: undefined,
+        });
+        const newuser2 = await UserModel.create({
+            username: "colin", 
+            email: "colin2@gmail.com", 
+            password: "12345678", 
+            userRole: "Candidate",
+            education: ["University of Manitoba"],
+            skills: ["Kendama"],
+            experience: ["Software Engineering 2"],
+            companyDetails: undefined,
+            hiringDetails: undefined,
+        });
+        let user = await UserModel.findById(newuser._id);
+        let user2 = await UserModel.findById(newuser2._id);
+        // Check record is in database
+        expect(user).not.toBeNull();
+        expect(user2).not.toBeNull();
+        const preference = 2;
+        const mReq1: Partial<Request> = {
+            body: {preference: preference},
+            userId: newuser._id,
+        };
+        const mReq2: Partial<Request> = {
+            body: {preference: preference},
+            userId: newuser2._id,
+        };
+        const mJson = jest.fn().mockImplementation(() => null)
+        const mStatus = jest.fn().mockImplementation(() => ({ json: mJson }))
+        const mRes: Partial<Response> = {
+            status: mStatus
+        };
+        const mNext = jest.fn();
+        await updateUserDetails(mReq1 as Request, mRes as Response, mNext);
+        await updateUserDetails(mReq2 as Request, mRes as Response, mNext);
+        await updateUserDetails(mReq1 as Request, mRes as Response, mNext);
+        await updateUserDetails(mReq2 as Request, mRes as Response, mNext);
+        const findUpdatedUser = await UserModel.findById(newuser._id);
+        const findUpdatedUser2 = await UserModel.findById(newuser2._id);
+        expect(findUpdatedUser?.preferences[0]).toBe(0);
+        expect(findUpdatedUser?.preferences[1]).toBe(0);
+        expect(findUpdatedUser?.preferences[2]).toBe(2);
+        expect(findUpdatedUser?.preferences[3]).toBe(0);
+        expect(findUpdatedUser?.preferences[4]).toBe(0);
+        expect(findUpdatedUser?.preferences[5]).toBe(0);
+        expect(findUpdatedUser2?.preferences[0]).toBe(0);
+        expect(findUpdatedUser2?.preferences[1]).toBe(0);
+        expect(findUpdatedUser2?.preferences[2]).toBe(2);
+        expect(findUpdatedUser2?.preferences[3]).toBe(0);
+        expect(findUpdatedUser2?.preferences[4]).toBe(0);
+        expect(findUpdatedUser2?.preferences[5]).toBe(0);
+    });
+
+    //INTEGRATION TESTS START
+
+    test('Check if sort behaves OK on single job', async () => {
         const newuser = await UserModel.create({
             username: "colin", 
             email: "colin@gmail.com", 
