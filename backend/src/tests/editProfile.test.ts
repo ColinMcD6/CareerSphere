c7072e89a98da55833dbe32e824f670c963ad918
EthanLapkin <126340807+EthanLapkin@users.noreply.github.com>
2025-03-27 22:49:46 -0500

    fix faulty test

--- backend/src/tests/editProfile.test.ts
+++ backend/src/tests/editProfile.test.ts
@@ -472,7 +472,7 @@ describe('Test candidate and employer portals', () => {
         const mNext = jest.fn();
         await getUserHandler(mReq as Request, mRes as Response, mNext);
         let error: Error = mNext.mock.calls[0][0];
-        expect(error.message).toContain("User does not exist, invalid ID!");
+        expect(error.message).toContain("User does not exist !");
     });
 
     test('Get existing user and make sure password is not contained in response body', async () => {
